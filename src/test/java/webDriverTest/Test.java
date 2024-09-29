e732e1fdecccfadc11ad1e6dd50072d66b49307c
nkammaragit <naveen.qtp@gmail.com>
2024-09-29 08:15:21 +0530

    test

--- src/test/java/webDriverTest/Test.java
+++ src/test/java/webDriverTest/Test.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
-//     @Test
+//     @Test  test test
 //	public void test1() {
 //		System.out.println("TestNg first test");
 //	}
